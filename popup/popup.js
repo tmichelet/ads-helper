af6bd29581d4b9d9057b004aa4b43136a7c68cf3
tmi <michelet.tristan@gmail.com>
2013-06-02 20:43:20 +0200

    working...

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,10 @@
 function clickHandler(e) {
-    chrome.extension.sendMessage({directive: "popup-click"}, function(response) {
+	console.log('test all clicked')
+    chrome.extension.sendMessage({directive: "test-all"}, function(response) {
         this.close(); // close the popup when the background finishes processing request
     });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('click-me').addEventListener('click', clickHandler);
+    document.getElementById('test-all').addEventListener('click', clickHandler);
 })
