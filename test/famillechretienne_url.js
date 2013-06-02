9e0e1dbefa7c46880460fd79be3499a00c3480c8
tmi <michelet.tristan@gmail.com>
2013-06-02 20:43:33 +0200

    working

---
+++ test/famillechretienne_url.js
@@ -0,0 +1,3 @@
+list = 'http://www.famillechretienne.fr/rsultats-de-recherche-pa_138.php?RID=138&search_rub=15&search_srub=17&search_category=2&keywords=&search_budget=1200&search_surface=40&search_pieces=2&search_pays=1&search_region=22&depts%5B%5D=91&depts%5B%5D=92&depts%5B%5D=75&depts%5B%5D=77&depts%5B%5D=93&depts%5B%5D=95&depts%5B%5D=94&depts%5B%5D=78&x=37&y=11'
+
+detail = 'http://www.famillechretienne.fr/dtails_118.php?pan_idt=71386'
